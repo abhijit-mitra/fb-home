d099f9d20587df93b53945138648e54e51f0ae6d
Abhijit Mitra <abhijitmitra.in@gmail.com>
2020-07-03 16:21:03 +0530

    updated

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import React, {useEffect} from 'react';
 import PropTypes from 'prop-types';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Link} from 'react-router-dom';
 import {Home, Payouts} from './components/pages';
 import {setUser} from './actions';
 import {connect} from 'react-redux';
 
-let name = 'Abhijit Mitra';
+const Home_1 = ()=>(<h1>Home</h1>);
+
+const About = ()=>(<h1>About</h1>);
+
+const Career = ()=>(<h1>Career</h1>);
 
 
 const App = ({setUser}) => {
@@ -14,11 +18,33 @@ const App = ({setUser}) => {
   // eslint-disable-next-line
   }, []);
   return (
-    <div className='w-100'>
-      <Switch>
-        <Route exact path="/payouts" component={Payouts} />
-        <Route exact path="/" component={Home} />
-      </Switch>
+    <div className='container'>
+        <div className='w-100 py-3 border br-10 mb-5'>
+        <div className='row'>
+        <div className='col-md-2 d-flex justify-content-center'>
+          <Link to={'/'}>
+            Home
+          </Link>
+        </div>
+        <div className='col-md-2'>
+          <Link to={'/about'}>
+            About
+          </Link>
+        </div>
+        <div className='col-md-2'>
+          <Link to={'/career'}>
+            Career
+          </Link>
+        </div>
+        </div>
+      </div>
+      <div className='mb-5'>
+        <Switch>
+          <Route exact path="/" component={Home_1} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/career" component={Career} />
+        </Switch>
+      </div>
     </div>
   );
 };
