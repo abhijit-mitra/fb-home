94617ecdd02247e2d670a067ccb59e09dc084514
Ashik Mitra <ashikmitra.cs@gmail.com>
2020-07-04 20:50:30 +0530

    Modified

--- src/components/pages/Login/index.js
+++ src/components/pages/Login/index.js
@@ -8,19 +8,21 @@ const Login = () => (
       <div className="col-10 offset-1">
         <div className="row">
           <div className="col-6">
-            <div className="fb m-2 border p-5 d-flex justify-content-center ">
+            <div className="fb m-4 border p-5 d-flex justify-content-center ">
               <b>Facebook</b>
             </div>
           </div>
           <div className="col-6">
-            {data.map((elm, a) => (
-              <div
-                className="col-5 border p-4 m-4 name d-flex justify-content-center"
-                key={a}
-              >
-                {elm}
-              </div>
-            ))}
+            <div className="row">
+              {data.map((elm, a) => (
+                <div
+                  className="col-4 border p-4 m-4 name d-flex justify-content-center"
+                  key={a}
+                >
+                  {elm}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
       </div>
