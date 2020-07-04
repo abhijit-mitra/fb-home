1ac944721ef3506400cd03a1fb7b1d6e0f78116a
Ashik Mitra <ashikmitra.cs@gmail.com>
2020-07-04 20:20:48 +0530

    integrated eslint

--- src/app.js
+++ src/app.js
@@ -1,56 +1,13 @@
-import React, {useEffect} from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import {Switch, Route, Link} from 'react-router-dom';
-import {Home, Payouts} from './components/pages';
-import {setUser} from './actions';
-import {connect} from 'react-redux';
+import Login from './components/pages/Login';
 
-const Home_1 = ()=>(<h1>Home</h1>);
-
-const About = ()=>(<h1>About</h1>);
-
-const Career = ()=>(<h1>Career</h1>);
-
-
-const App = ({setUser}) => {
-  useEffect(()=>{
-    setUser();
-  // eslint-disable-next-line
-  }, []);
+const App = (props) => {
   return (
-    <div className='container'>
-        <div className='w-100 py-3 border br-10 mb-5'>
-        <div className='row'>
-        <div className='col-md-2 d-flex justify-content-center'>
-          <Link to={'/'}>
-            Home
-          </Link>
-        </div>
-        <div className='col-md-2'>
-          <Link to={'/about'}>
-            About
-          </Link>
-        </div>
-        <div className='col-md-2'>
-          <Link to={'/career'}>
-            Career
-          </Link>
-        </div>
-        </div>
-      </div>
-      <div className='mb-5'>
         <Switch>
-          <Route exact path="/" component={Home_1} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/career" component={Career} />
+          <Route exact path="/" component={Login} />
         </Switch>
-      </div>
-    </div>
   );
 };
 
-App.propTypes = {
-  setUser: PropTypes.func,
-};
-
-export default connect(null, {setUser})(App);
+export default App;
---
+++ src/components/pages/Login/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './index.css';
+const data = ['Abhi', 'Ashik', 'Abhijit', 'Aryan'];
+
+const Login = () => (
+  <div className="container-fluid">
+    <div className="row">
+      <div className="col-10 offset-1">
+        <div className="row">
+          <div className="col-6">
+            <div className="fb m-2 border p-5 d-flex justify-content-center ">
+              <b>Facebook</b>
+            </div>
+          </div>
+          <div className="col-6">
+            {data.map((elm, a) => (
+              <div
+                className="col-5 border p-4 m-4 name d-flex justify-content-center"
+                key={a}
+              >
+                {elm}
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Login;
